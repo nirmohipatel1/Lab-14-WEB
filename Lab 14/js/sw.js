d4f0c6f654f971be709b5acb48084879b2046d43
nirmohipatel1 <56351358+nirmohipatel1@users.noreply.github.com>
2019-11-26 00:02:54 -0500

    offline web application Lab 14

---
+++ Lab 14/js/sw.js
@@ -0,0 +1,14 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+workbox.precaching.precacheAndRoute([
+    '/index.htm',
+    '/js/index.js',
+    '/img/add.svg'
+]);
+
+workbox.routing.registerRoute(
+    /\.htm(l?)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: 'html-cache',
+    })
+);
